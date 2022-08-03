8720b05cbeb4fe4e2436e98302bd71699e265153
zhangpeng <31369372+stubborn123@users.noreply.github.com>
2022-08-03 13:56:50 +0800

    关于一个springboot整合flowable
    关于spring整合flowable：
    依赖引入
    数据库对应的表
    一个简单的流程文件和对应请求测试

---
+++ flowable/0803_flowable_demo/src/main/java/com/example/flowable/demo/ZpFlowableApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowable.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpFlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZpFlowableApplication.class, args);
+    }
+
+}
